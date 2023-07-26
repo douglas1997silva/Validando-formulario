49ed6db625c4606caf5ca06c8b55e29be1a74375
douglas <douglas.olliveira1997@gmail.com>
2023-07-26 20:29:10 -0300

    finalizado a validação do cpf

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,9 +1,11 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g,"");
-    validaNumeroRepetidos(cpf);
-    console.log(validaNumeroRepetidos(cpf))
+    if(validaNumeroRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
+        console.log('Esse cpf não existe')
+    } else {
+        console.log('EXISTE')
+    }
 }
-
 function validaNumeroRepetidos(cpf){
     const numeroRepetidos = [
         '00000000000',
@@ -19,4 +21,38 @@ function validaNumeroRepetidos(cpf){
     ]
 
      return numeroRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0 ; tamanho < 9 ;tamanho ++){
+        soma+= cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 ||soma == 11){
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let tamanho = 0 ; tamanho < 10 ;tamanho ++){
+        soma+= cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 ||soma == 11){
+        soma = 0;
+    }
+console.log(soma)
+    return soma != cpf[10];
 }
